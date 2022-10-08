9fca71cd7be139dba816e0975f0cf9649c034adf
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-07 22:03:57 -0300

    sexto commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './components/Hero.js';
+import Features from './components/Features';
 
 const App = () => {
   return (
     <div className='w-full max-w-[1440px] mx-auto bg-white'>
       <Hero/>
+      <Features/>
     </div>
   )
 }
