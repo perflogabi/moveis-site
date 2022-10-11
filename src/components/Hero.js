23f4677c0f6be86764ec362f99cb26d3617982ea
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-11 19:27:58 -0300

    nono commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,35 @@
 import React from 'react';
+
+// import data
 import { hero } from '../data';
-import  Stats from '../components/Stats';
+
+// import components
+import Stats from './Stats';
 
 const Hero = () => {
-    const {title, subtitle, buttonText} = hero;
-    return (
-        <section className='h-[850px] w-full bg-hero bg-right bg-cover bg-no-repeat text-white'>
-          <div className='container mx-auto text-center'>
-            <h1 className='text-2x1 mx-auto font-semibold mb-[30px] 1g:text-[64px] 1g:leading-tight 1g:max-w-[888px]'>{title}</h1>
-            <h2 className='mb-[30px] max-w-[627px] max-auto 1g:mb-[65px] 1g:text-x1'>{subtitle}</h2>
-            <button className='bg-[rgba(255,255,255, 0.4) hover:bg-[rgba(255,255,255, 0.5)] px-[35px] py-[9px] mb-[160px] text-x1 rounded-md backdrop-blur-md transition 1g:px-[80px] 1g:mb-[194px]'>{buttonText}</button>
-            <div>
-              <Stats />
-            </div>
-          </div>  
-        </section>
-        
-    )
-}
+  const { title, subtitle, buttonText } = hero;
+  return (
+    <section className='h-full max-h-[850px] w-full bg-hero bg-right lg:bg-center bg-cover bg-no-repeat text-white pt-[225px] pb-[254px] relative mb-12 lg:mb-28'>
+      <div className='container mx-auto text-center'>
+        {/* title */}
+        <h1 className='text-2xl lg:text-[64px] lg:leading-tight lg:max-w-[888px] mx-auto font-semibold mb-[30px]'>
+          {title}
+        </h1>
+        {/* subtitle */}
+        <h2 className='mb-[30px] lg:mb-[65px] max-w-[627px] mx-auto lg:text-xl'>
+          {subtitle}
+        </h2>
+        {/* button */}
+        <button className='px-[35px] lg:px-[80px] py-[9px] lg:py-[16px] mb-[160px] lg:mb-[194px] text-xl rounded-md bg-[rgba(255,255,255,0.4)] hover:bg-[rgba(255,255,255,0.5)] backdrop-blur-md transition'>
+          {buttonText}
+        </button>
+        {/* stats */}
+        <div className='-top-[70px] relative'>
+          <Stats />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Hero;
