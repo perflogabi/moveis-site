143e9dbe7cea06facab3070f14cf501b9d602be7
Gabriela Florêncio <perflogabi@gmail.com>
2022-10-11 22:18:43 -0300

    decimo commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Hero from './components/Hero.js';
 import Features from './components/Features';
 import NewItems from './components/NewItems';
+import FeaturesSecond from './components/FeaturesSecond'
+import Products from './components/Products'
 
 const App = () => {
   return (
     <div className='w-full max-w-[1440px] mx-auto bg-white'>
-      <Hero/>
-      <Features/>
-      <NewItems/>
+      <Hero />
+      <Features />
+      <NewItems />
+      <FeaturesSecond />
+      <Products />
     </div>
   )
 }
